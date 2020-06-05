8702cb99ee9352b5753fb449105d632b01bbf6fe
srft <64444122+srft@users.noreply.github.com>
2020-06-05 04:13:18 +0200

    Add files via upload

---
+++ Kullanici girisli sayilar carpimi.py
@@ -0,0 +1,19 @@
+def multiply():
+    
+    print("The First Number:")
+    
+    num1 = int(input())
+    
+    print("The Second Number:")
+    
+    num2 = int(input())
+    
+    print("The Third Number:")
+    
+    num3 = int(input())
+    
+    print("The Result:")
+    
+    print(num1 * num2 * num3)
+    
+multiply()
