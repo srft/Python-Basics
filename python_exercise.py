aa82f941a1c24027c020635380c313a928d292b3
srft <64444122+srft@users.noreply.github.com>
2020-06-06 15:40:22 +0200

    Add files via upload

---
+++ python_exercise.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 22:31:44 2020
+
+@author: Serafettin Yildirim
+"""
+
+
+isim = input("İsminiz nedir? ")
+
+print("Merhaba", isim, end="!\n")
+
+
+
+
+yas = input("yasiniz?")
+
+print("Demek", yas, "yasindasiniz!")
+
+# dairenin alani
+
+cap = input("Dairenin capi:") 
+yaricap = int(cap) / 2
+pi = 3.14
+print("dairenin alani:", pi * (yaricap ** 2))
+
+
+
+
+# tip donusum
+
+sayi = input("bir veri girin:")
+tip = type("sayi")
+print("girdiginiz verinin tipi:", tip)
+
