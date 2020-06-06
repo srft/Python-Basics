73dee89c09999d58b01e6526651c61b96959faab
srft <64444122+srft@users.noreply.github.com>
2020-06-06 16:37:26 +0200

    Add files via upload

---
+++ python_exercise2.py
@@ -0,0 +1,168 @@
+print("hello")
+
+9*2
+
+9.2 float
+9.2
+type(123)
+type("ali")
+"123"
+len("merhabalar")
+len("serafettin")
+
+
+hello = "merhabalar"
+hello
+
+
+len(hello)
+len("hello")
+
+#del hello
+
+len(hello)
+
+
+hello.upper()
+hello.islower()
+
+#replace
+
+hello
+hello= hello.replace("l","p")
+hello
+dir(hello)
+hello.isdigit()
+"123".isdigit()
+a = "123"
+a.isdigit()
+hello.title()
+
+hello.capitalize()
+
+
+hi = "Hello AI ERA"
+
+hi[6]
+hi[6:8]
+hi[9:12]
+
+#DEGISKENLER
+
+a = 9
+a
+print(a)
+b = 10 
+a*b
+
+
+#KULLANICIDAN BILGI ALMA & TIP DONUSUMLERI
+number = input()
+
+number*3
+
+type(number)
+int(number)
+
+num1 = int(input())
+num2 = int(input())
+
+
+num1 + num2
+
+def topla():
+    
+    print("birinci sayiyi giriniz:")
+    
+    num1 = int(input())
+    
+    print("ikinci sayiyi giriniz:")
+    
+    num2 = int(input())
+    
+    print("islem sonucu:")
+    
+    print(num1 + num2)
+    
+topla()
+    
+    
+    
+ # VERI YAPILARI
+
+## LISTELER
+
+notlar = [1,2,3,4]   
+    
+type(notlar) 
+
+notlar[0:2]   
+    
+    
+names = ["python","R",1,2,3]    
+    
+names[3]    
+len(names)    
+
+
+all_list = [names, 3,4,5]
+
+
+all_list[0]
+
+# LISTE ELEMAN ISLEMLERI
+
+list_names = ["ali","veli","berk","ayse"]
+
+list_names[1] = "velinin_babasi"
+
+
+list_names.append("ahmet")
+
+list_names
+
+# remove
+
+list_names.remove("ahmet")
+
+list_names.insert(0, "mehmet")
+
+list_names[3] = "berkcan"
+
+list_names.pop(0)
+
+list_names
+
+dir(list_names)
+
+
+list_names.copy()
+
+list_names.index("veli")
+
+
+
+sozluk = {"reg": "regresyon modeli","LOG": "logistik"}
+
+len(sozluk)
+
+sozluk["reg"]
+
+
+hi = "merhabalar hepinize"
+len(hi)
+hi = hi.replace("a","e")
+hi
+hi[6:]
+hi[0:14]
+
+
+hi.sort()
+
+dir(hi)
+
+
+
+
+
+    
